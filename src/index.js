633a3c5e507b6e99a8e854b5e25ca9bdfc058dc1
kizzylion <kztchm@gmail.com>
2024-05-14 00:53:55 -0400

    HTML for the Weather app

--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
-import _ from "lodash";
 import "./style/style.css";
-import { Reminder } from "./reminder";
-import '@fortawesome/fontawesome-free/css/all.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import { renderUi } from "./renderMainUi.js";
-import { showAddGroupModal } from "./addgroup.js";
-import { renderNavUi } from "./rendernav.js";
-import { showTaskModal } from "./addTask.js";
-import {initialRender, viewAllTask, viewTaskByGroup } from "./domevents.js";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
+import videolink from "./img/video.mp4";
+import logolink from "./img/Logo.png";
+import moon from "./img/moon.png";
+import hourlyCond from "./img/weather-icon-clear.svg";
 
-export const taskMaster = new Reminder();
-taskMaster.checkLocalStorage();
+const video = document.getElementById("bgvideo");
+const logo = document.querySelector(".logo img");
+const moondimg = document.querySelector("#moonimg");
+const hourlyImg = document.querySelectorAll(".hourlyImg");
+const city = document.querySelectorAll(".cityBackground");
+console, console.log(city);
 
-initialRender()
-
-
-
-
-
-    
-
-    
-
-
-
-
-
-
-
+window.onload = function () {
+  video.src = videolink;
+  logo.src = logolink;
+  moonimg.src = moon;
+  hourlyImg.forEach((e) => (e.src = hourlyCond));
+  if (city.length) {
+    city.forEach((e) => (e.src = videolink));
+  }
+};
--- webpack.common.js
+++ webpack.common.js
@@ -4,9 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: {
     index: "./src/index.js",
-    themescript: "./src/themescript.js",
     // print: "./src/print.js",
-  
   },
   output: {
     filename: "[name].bundle.js",
@@ -19,14 +17,24 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
         type: "asset/resource",
         generator: {
           filename: "images/[name][ext]",
+        },
+      },
+      {
+        test: /\.(mp4|webm)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "videos/",
+          },
         },
       },
       {
@@ -41,20 +49,20 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      title: "KizzTaskMaster App",
+      title: "Yo! Weather App",
       favicon: "./src/img/Logo.png",
       inject: "body",
       meta: {
         viewport: "width=device-width, initial-scale=1.0",
-        "og:title": "KizzTaskMaster App",
-        "og:description": "KizzTaskMaster App",
+        "og:title": "Yo! Weather App",
+        "og:description": "Yo! Weather App",
         "og:image": "./images/Logo.png",
         "og:type": "website",
-        "og:url": "https://kizzylion.github.io/kizztaskmaster/",
+        "og:url": "",
         "twitter:card": "summary_large_image",
         "twitter:image": "./images/Logo.png",
-        "twitter:title": "KizzTaskMaster App",
-        "twitter:description": "KizzTaskMaster App",
+        "twitter:title": "Yo! Weather App",
+        "twitter:description": "Yo! Weather App",
       },
     }),
   ],
